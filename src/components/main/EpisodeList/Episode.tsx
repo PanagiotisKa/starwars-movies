19fda9418fbac8e30ea7f5cdd9696048b7f0edce
Panagiotis Kakaletris <kakaletris.panagiotis@gmail.com>
2024-06-08 14:57:20 +0300

    refactoring to Episode table cell

--- src/components/main/EpisodeList/Episode.tsx
+++ src/components/main/EpisodeList/Episode.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { TableRow, TableCell } from "@mui/material"
+type EpisodeType = {
+  episode: {
+    episode_id: number
+    title: string
+    opening_crawl: string
+    release_date: string
+  }
+}
 
-function Episode() {
+
+function Episode({ episode }: EpisodeType) {
   return (
-    <div>Episode</div>
+    <TableRow key={episode.episode_id}   onClick={() => console.log("ok")}>
+    <TableCell>EPISODE {episode.episode_id} </TableCell> 
+    <TableCell>{episode.title} </TableCell> 
+    <TableCell>{episode.release_date? new Date(episode.release_date).toLocaleString('el', {year: 'numeric', month: 'numeric', day: 'numeric'}): '-'} </TableCell>  
+ </TableRow>
   )
 }
 
--- src/components/main/EpisodeList/EpisodeList.tsx
+++ src/components/main/EpisodeList/EpisodeList.tsx
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import  { useDispatch, useSelector } from 'react-redux'
 import { getEpisodeListData } from '../../../state/episodeListSlice'
 import { RootState, AppDispatch } from '../../../state/store'
+import { Box, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material'
+import Episode from './Episode'
 
 function EpisodeList() {
-
-  const [finalArray, setFinalArray] = useState([])
 
     const dispatch = useDispatch<AppDispatch>()
     const { value, loading, error } = useSelector((state: RootState) => state.episodeList)
@@ -21,18 +21,18 @@ function EpisodeList() {
     if (error) return <div>Error: {error}</div>;
     if(value) {
       return (
-        <>
-        <ul>
+        <Box sx={{minWidth: 300}}>
+        <Table>
+          <TableBody>
         {[...value].sort((a,b)=>{ 
           if( sort === 'year') { return parseInt(a.release_date.split("-")[0]) - parseInt(b.release_date.split("-")[0])} 
-          return a.episode_id - b.episode_id}).filter(episode => episode.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map(episode => {
-          return (
-            <li key={episode.episode_id}> EPISODE {episode.episode_id} {episode.title} {episode.release_date}</li>
-          )
+          return a.episode_id - b.episode_id}).filter(episode => episode.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+          .map(episode => {
+          return (<Episode episode={episode}/>)
         })}
-        </ul>
-        <div>episodeList</div>
-        </>
+        </TableBody>
+        </Table>
+        </Box>
       )
     }
 }
